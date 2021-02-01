44283441c2fd38f7e6ceb6a40ce6db7875eb061f
Jangsea Park <jangsea.park@gmail.com>
2021-02-01 23:20:51 +0000

    Report the results

--- write.py
+++ write.py
@@ -25,7 +25,11 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for approach in results:
+            writer.writerow(approach.serialize() | approach.neo.serialize())
 
 
 def write_to_json(results, filename):
@@ -39,4 +43,11 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    approaches = list()
+    for approach in results:
+        dict_approch = approach.serialize()
+        dict_approch['neo'] = approach.neo.serialize()
+        approaches.append(dict_approch)
+
+    with open(filename, 'w') as f:
+        writer = json.dump(approaches, f, indent=2)
